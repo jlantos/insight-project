33800fbc772b20ee5ab247ba440a046d3ad733e8
Judit Lantos <lantosjudit@gmail.com>
2016-06-07 14:03:02 -0700

    Added common stream, fixed room info frequency

--- create_data.py
+++ create_data.py
@@ -5,7 +5,8 @@ import json
 
 
 def main():
-  """Simulates sensor and location data (in 2 separate streams)."""
+  """Simulates sensor and location data (both in the same and 
+     in 2 separate streams)."""
   if len(sys.argv) != 3:
     print "Usage: ./create_data.py number_of_users full_time_mins"
     sys.exit(1)
@@ -17,6 +18,7 @@ def main():
   # Output for sensor and location datastream
   badge_output = open('badge_stream', "w")
   sensor_output = open('sensor_stream', "w")
+  badge_sensor_output = open('badge_sensor_stream', "w")
 
   # Set random state
   random.seed(1)
@@ -39,14 +41,18 @@ def main():
     # Follow the n users
     for user_id in range (0, n_users):
 
+      location_changed = False
+
       ### Select room ###
       # Initial location is randomly selected
       if t == 0: 
         new_location = random.randint(0, 9)
+        location_changed = True
       else:
         # User changes room twice per hour in average
         if random.random() < 2.0/60:
           new_location = rooms[old_location[user_id]][random.randint(0,len(rooms[old_location[user_id]])-1)]
+          location_changed = True
         # Or stays in the same room
         else:
           new_location = old_location[user_id]
@@ -64,21 +70,42 @@ def main():
       loc_data = {"room": {"userid": user_id,  "timestamp": t, "newloc": new_location, "oldloc": old_location[user_id]}}
       sens_data = {"sensor": {"userid": user_id,  "timestamp": t, "doserate": rate}}
 
-      # Write the info for this minute to file   
-      json.dump(loc_data, badge_output)
-      badge_output.write("\n")
+      # Write the info for this minute to file
+      if location_changed:   
+        json.dump(loc_data, badge_output)
+        badge_output.write("\n")
+
+        json.dump(loc_data, badge_sensor_output)
+        badge_sensor_output.write("\n")
 
       json.dump(sens_data, sensor_output)
       sensor_output.write("\n")
+
+      json.dump(sens_data, badge_sensor_output)
+      badge_sensor_output.write("\n")
 
       # Keep track of previous room
       old_location[user_id] = new_location
       # print t, user_id, new_location, rate
 
+
+  # Send everybody home
+  new_location = 100
+  t = t + 1
+  for user_id in range (0, n_users):
+    loc_data = {"room": {"userid": user_id,  "timestamp": t, "newloc": new_location, "oldloc": old_location[user_id]}}
+    json.dump(loc_data, badge_output)
+    badge_output.write("\n")
+
+    json.dump(loc_data, badge_sensor_output)
+    badge_sensor_output.write("\n")
   
+
+
   # Close files
   badge_output.close()
   sensor_output.close()
+  badge_sensor_output.close()
 
 
 # Timing run
