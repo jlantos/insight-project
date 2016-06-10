dac4c32f9e390ba2ea5ffca43a0a6a5a762dc972
Ubuntu <ubuntu@ip-172-31-1-104.us-west-2.compute.internal>
2016-06-10 23:51:41 +0000

    Raw sensor graph fixed

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from flask import jsonify
+from flask import render_template
 from app import app
 from cassandra.cluster import Cluster
-
+from flask import make_response
+import json
 
 cluster = Cluster(['52.35.6.29', '52.35.6.29', '52.35.6.29', '52.24.174.234'])
 session = cluster.connect('raw_data')
@@ -12,7 +14,9 @@ session = cluster.connect('raw_data')
 
 
 def index():
-  return "Hello, World!"
+  user = { 'nickname': 'Miguel' } # fake user
+  mylist = [1,2,3,4]
+  return render_template("index.html", title = 'Home', user = user, mylist = mylist)
 
 
 @app.route('/api/<userid>')
@@ -23,5 +27,9 @@ def get_email(userid):
        response_list = []
        for val in response:
             response_list.append(val)
-       jsonresponse = [{ "User ID": x.user_id, "time": x.timestamp, "dose rate": x.rate} for x in response_list]
-       return jsonify(rates=jsonresponse)
+      # jsonresponse = [{ "User ID": x.user_id, "time": x.timestamp, "dose rate": x.rate} for x in response_list]
+       #return jsonify(rates=jsonresponse)
+       jsonresponse = [{"time": x.timestamp, "dose_rate": x.rate} for x in response_list]
+       return render_template("line_graph.html", jsondata = (json.dumps(jsonresponse))) 
+       #return render_template("line_graph.html", jsondata = (jsonify(stream={"r":jsonresponse})))
+
