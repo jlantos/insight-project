3c0814bd10b81ee23d090e82821d797c993284be
Judit Lantos <lantosjudit@gmail.com>
2016-06-21 11:54:21 -0700

    Started last room sum query

--- app/views.py
+++ app/views.py
@@ -4,6 +4,8 @@ from app import app
 from cassandra.cluster import Cluster
 from flask import make_response
 import json
+from neo4jrestclient.client import GraphDatabase
+
 
 cluster = Cluster(['52.35.6.29', '52.35.6.29', '52.35.6.29', '52.24.174.234'])
 session = cluster.connect('rata_data')
@@ -65,4 +67,29 @@ def get_room_sum(userid):
 
        jsonresponse = [{"time": x.timestamp, "dose_rate": x.sum_rate} for x in response_list]
        return render_template("line_graph_sum.html", jsondata = (json.dumps(jsonresponse)))
+
+
+@app.route('/api/room_notification/<num_rooms>')
+def get_room_alerts(num_rooms):
+       db = GraphDatabase("http://ec2-52-40-124-21.us-west-2.compute.amazonaws.com:7474")
+
+       for room in range(0, num_rooms):
+         stmt = "SELECT * FROM room_sum WHERE room_id={0} LIMIT 1 ALLOW FILTERING".format(room)
+         response = session.execute(stmt)
+         response_list = []
+         for val in response:
+            response_list.append(val)
+
+         #if response["sum_rate"] > 80:
+         #   first = "$MATCH (room { number:'" + str(room) "'})-[:GATE]-(first_con) RETURN first_con"
+         #   second = "$MATCH (room { number:'" + str(room) "'})-[:GATE*2]-(sec_con) RETURN room, sec_con"
+
+         #   results = db.query(first, returns=(str))
+         #   for r in results:
+         #     print(r[0])
+
+
+       jsonresponse = [{"time": x.timestamp, "dose_rate": x.sum_rate} for x in response_list]
+       return jsonify(rates=jsonresponse)
+
 
