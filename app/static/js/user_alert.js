04b537bf4cac9fe3e7dff3811272bec95c6e754d
Ubuntu <ubuntu@ip-172-31-1-104.us-west-2.compute.internal>
2016-06-27 00:40:16 +0000

    User alert works

---
+++ app/static/js/user_alert.js
@@ -0,0 +1,36 @@
+function getData_for_user_alert() {
+    $.get("/api/user_notification/1000_100", function(graph) {
+       // console.log(graph.dose_rates)
+       // updateRoomAlert(graph.alerts)
+       myFunction2(graph.avg_time, graph.alerts)
+    });
+};
+
+
+setInterval(getData_for_user_alert, 1000);
+
+
+
+function myFunction2(alert_time, response) {
+    var time = alert_time
+    var arr = response;
+    var i;
+    var out = "<table style=\"width:100%\">";
+
+    out += "<tr><th>Time</th><th>User in danger</td><th>Closest colleague</th><th>Distance</th> </tr>"
+
+    for(i = 0; i < arr.length; i++) {
+        out += "<tr><td>" +
+        time.toString() + 
+        "</td><td>" +
+        arr[i].user_in_danger +
+        "</td><td>" +
+        arr[i].user_to_alert +
+        "</td><td>" +
+        arr[i].distance +
+        "</td></tr>";
+    }
+    out += "</table>";
+    document.getElementById("id00").innerHTML = out;
+}
+
